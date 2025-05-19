f0332eda65f2bbf6a0ba4c52caa1799c9d45abce
Paulo Silva <paulobfsilva@gmail.com>
2025-05-19 16:33:10 +0100

    Initial Commit

---
+++ ChurchHymnTests/ChurchHymnTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChurchHymnTests.swift
+//  ChurchHymnTests
+//
+//  Created by paulo on 19/05/2025.
+//
+
+import Testing
+@testable import ChurchHymn
+
+struct ChurchHymnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
